a7fd01d3d1480a73143dbe03d79ab758319152fe
yaoyaoloveqiaoqiao <632212519@qq.com>
2020-08-29 00:32:35 +0800

    codec

--- app/src/main/java/com/yao/yaoplayerandroid/MySurfaceView.java
+++ app/src/main/java/com/yao/yaoplayerandroid/MySurfaceView.java
@@ -48,9 +48,9 @@ public class MySurfaceView extends SurfaceView implements SurfaceHolder.Callback
         //String videoPath = dir.getAbsolutePath() + "/" + "ST/time_clock_1min_720x1280_30fps.mp4";
         //String videoPath = dir.getAbsolutePath() + "/" + "ST/ads.mp4";
         //String videoPath = dir.getAbsolutePath() + "/" + "ST/banfo.mp4";
-        String videoPath = dir.getAbsolutePath() + "/" + "ST/The_Beauty_of_Earth.mp4";
+        //String videoPath = dir.getAbsolutePath() + "/" + "ST/The_Beauty_of_Earth.mp4";
         //String videoPath = dir.getAbsolutePath() + "/" + "ST/4k_animal.mp4";
-        //String videoPath = dir.getAbsolutePath() + "/" + "ST/rabbit.mp4";
+        String videoPath = dir.getAbsolutePath() + "/" + "ST/rabbit.mp4";
         System.out.println("+++++++++path:" + videoPath);
 
         File f = new File(videoPath);
